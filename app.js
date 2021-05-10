03ce6f3d5e327b6a5e2b375c542c647b6f361427
Satyam Sovan Mishra <satyamsovan123@Satyams-MacBook-Air.local>
2021-05-10 14:53:22 +0530

    Inital commit

--- app.js
+++ app.js
@@ -168,6 +168,6 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server is running on PORT 3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running");
 });
